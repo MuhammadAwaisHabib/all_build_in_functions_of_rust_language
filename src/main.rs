627e331a50bb596b7f8d56f3c4e1fb1447a5c73d
MuhammadAwaisHabib <muhawaishabib@gmail.com>
2020-06-17 23:18:45 +0500

    BI_functions

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+// Here I have all the build in functions that I know abuot and I use in codding 
+fn main() {
+    println!("no.1 = .clone(); ::: it clones a variable");
+    println!("no.2 = .push(); ::: it pushes Strings of data into a varuiable/vector");
+    println!("no.3 = .pop(); ::: it cuts a block of data from variables/vectors");
+    println!("no.4 = .new(); ::: it creats a new hashmap code");
+    println!("no.5 = .insert(); ::: it inserts data into a hashmap");
+    println!("no.5 = .collect(); ::: it it collets and compiles two or more inbuild fuctons");
+    println!("no.5 = .get(); ::: it brings variable no. 2 into variable no.1");
+    println!("no.5 = .iter(); ::: it is used with the name of the vector/varbale being combined");
+    println!("no.5 = .zip(); ::: it it combines a vector/variable with variable/vector");
+}
