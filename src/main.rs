384b9f0fae1238cad95243a306bfb6a03b302e08
MuhammadAwaisHabib <66966859+MuhammadAwaisHabib@users.noreply.github.com>
2020-06-18 00:47:30 +0500

    Main.rs
    these are the Build in functions of rust programming language that i use and know about.

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ fn main() {
     println!("no.3 = .pop(); ::: it cuts a block of data from variables/vectors");
     println!("no.4 = .new(); ::: it creats a new hashmap code");
     println!("no.5 = .insert(); ::: it inserts data into a hashmap");
-    println!("no.5 = .collect(); ::: it it collets and compiles two or more inbuild fuctons");
-    println!("no.5 = .get(); ::: it brings variable no. 2 into variable no.1");
-    println!("no.5 = .iter(); ::: it is used with the name of the vector/varbale being combined");
-    println!("no.5 = .zip(); ::: it it combines a vector/variable with variable/vector");
+    println!("no.6 = .collect(); ::: it it collets and compiles two or more inbuild fuctons");
+    println!("no.7 = .get(); ::: it brings variable no. 2 into variable no.1");
+    println!("no.8 = .iter(); ::: it is used with the name of the vector/varbale being combined");
+    println!("no.9 = .zip(); ::: it it combines a vector/variable with variable/vector");
 }
