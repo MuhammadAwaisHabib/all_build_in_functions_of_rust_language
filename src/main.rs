83a9437648939180bed64d8a05dd23266d329db0
Muhammad_Awais_Habib (a rust programmer) <66966859+MuhammadAwaisHabib@users.noreply.github.com>
2020-07-05 13:49:14 +0500

    latest

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 // Here I have all the build in functions that I know abuot and I use in codding 
 fn main() {
     println!("no.1 = .clone(); ::: it clones a variable");
-    println!("no.2 = .push(); ::: it pushes Strings of data into a varuiable/vector");
+    println!("no.2 = .push(); ::: it pushes Strings of data into a vector");
     println!("no.3 = .pop(); ::: it cuts a block of data from variables/vectors");
-    println!("no.4 = .new(); ::: it creats a new hashmap code");
+    println!("no.4 = .new(); ::: it creats a new code");
     println!("no.5 = .insert(); ::: it inserts data into a hashmap");
-    println!("no.6 = .collect(); ::: it it collets and compiles two or more inbuild fuctons");
+    println!("no.6 = .collect(); ::: it collets and compiles two inbuild fuctons in to a hashmap");
     println!("no.7 = .get(); ::: it brings variable no. 2 into variable no.1");
     println!("no.8 = .iter(); ::: it is used with the name of the vector/varbale being combined");
     println!("no.9 = .zip(); ::: it it combines a vector/variable with variable/vector");
